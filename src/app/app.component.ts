102fb488e67888252e21a5654443ac728cb53601
Ródney Antonius <rodney.agro@gmail.com>
2017-01-07 01:10:02 -0200

    splash screen resources updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { Platform, Nav } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
+import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { FirstRunPage } from '../pages/pages';
 
@@ -25,6 +25,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+      Splashscreen.hide();
     });
   }
 }
